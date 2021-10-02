67ee895c59c9fcb5f5809971518cb3290da17a80
Bart <bartvanlaar1997@gmail.com>
2021-10-02 16:55:14 +0200

    Working on abstract syntax tree

---
+++ Parser/AbstractSyntaxTree/ExpressionAST.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.AbstractSyntaxTree
+{
+    internal enum ExpressionType
+    {
+
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        LessThan,
+        MethodCall,
+        VariableEvaluation,
+        Prototype,
+        Function,
+        Number
+    }
+
+    internal abstract class ExpressionAST
+    {
+
+        public ExpressionAST(ExpressionType expressionType)
+        {
+            NodeExpressionType = expressionType;
+        }
+        public ExpressionType NodeExpressionType { get; }
+
+        protected internal virtual ExpressionAST VisitChildren(ExpressionVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+
+        protected internal virtual ExpressionAST Accept(ExpressionVisitor visitor)
+        {
+            return visitor.VisitExtension(this);
+        }
+    }
+
+    internal sealed class NumberExpressionAST : ExpressionAST
+    {
+        public double Value { get; }
+        public NumberExpressionAST(double value) : base (ExpressionType.Number)
+        {
+            Value = value;
+        }
+
+        protected internal override ExpressionAST Accept(ExpressionVisitor visitor)
+        {
+            return visitor.VisitNumberExpressionAST(this);
+        }
+    }
+
+}
---
+++ Parser/AbstractSyntaxTree/ExpressionVisitor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.AbstractSyntaxTree
+{
+    internal abstract class ExpressionVisitor
+    {
+        protected ExpressionVisitor()
+        {
+        }
+
+        public virtual ExpressionAST Visit(ExpressionAST node)
+        {
+            if (node != null)
+            {
+                return node.Accept(this);
+            }
+
+            return null;
+        }
+
+        protected internal virtual ExpressionAST VisitExtension(ExpressionAST node)
+        {
+            return node.VisitChildren(this);
+        }
+
+        protected internal virtual ExpressionAST VisitBinaryExpressionAST(BinaryExpressionAST node)
+        {
+            this.Visit(node.Lhs);
+            this.Visit(node.Rhs);
+
+            return node;
+        }
+
+        protected internal virtual ExpressionAST VisitCallExpressionAST(CallExpressionAST node)
+        {
+            foreach (var argument in node.Arguments)
+            {
+                this.Visit(argument);
+            }
+
+            return node;
+        }
+
+        protected internal virtual ExpressionAST VisitFunctionAST(FunctionAST node)
+        {
+            this.Visit(node.Proto);
+            this.Visit(node.Body);
+
+            return node;
+        }
+
+        protected internal virtual ExpressionAST VisitVariableExpressionAST(VariableExpressionAST node)
+        {
+            return node;
+        }
+
+        protected internal virtual ExpressionAST VisitPrototypeAST(PrototypeAST node)
+        {
+            return node;
+        }
+
+        protected internal virtual ExpressionAST VisitNumberExpressionAST(NumberExpressionAST node)
+        {
+            return node;
+        }
+    }
+}
